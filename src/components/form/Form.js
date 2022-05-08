cbf22f135de34459ebe09f2788352be98c1eb178
olga <yaolmi@yandex.ru>
2022-05-08 11:39:50 +0700

    remove formik

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,36 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useFormik } from 'formik';
 
 import './form.scss';
 
 const Form = ({main, formClass, inputClass, btnClass, updateWord}) => {
+    const [input, setInput] = useState('');
     let history = useHistory();
 
-    const formik = useFormik({
-        initialValues: {
-            feeling: ''
-        },
-        onSubmit: (values, {resetForm}) => {
-            if (values.feeling) {
-                history.push("/poem");
-            }
-            updateWord(values.feeling);
-            resetForm();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (input) {
+            history.push("/poem");
         }
-    });
+        updateWord(input);
+        setInput('');
+    }
 
     let btnText = main ? 'Search' : null;
     return (
-        <form onSubmit={formik.handleSubmit} action="#" className={`form ${formClass}`}>
+        <form onSubmit={handleSubmit} action="#" className={`form ${formClass}`}>
 			<input 
                 type="text" 
                 name="feeling" 
-                value={formik.values.feeling}
+                value={input}
                 placeholder="How are you feeling?" 
                 className={`form__input ${inputClass}`}
-                onChange={formik.handleChange} />
-			<button type="submit" disabled={!formik.values.feeling} className={btnClass}>{btnText}</button>
+                onChange={(e) => setInput(e.currentTarget.value)} />
+			<button type="submit" disabled={!input} className={btnClass}>{btnText}</button>
 		</form>
     )
 }
